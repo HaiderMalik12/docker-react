43bb62e0fc091cdd92bcb9c9ace184d6db014dff
HaiderMalik12 <haidermalik504@gmail.com>
2024-09-14 19:27:24 +0500

    changed app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React with me on how to deploy app
         </a>
       </header>
     </div>
